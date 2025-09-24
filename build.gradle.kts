fa7a2850538d329e2cf4ba1da27089c4f073cd77
NotAlexNoyle <alexnoyle@icloud.com>
2025-09-24 02:03:28 -0400

    Update gradle to 8.14.3 and shadow to 8.3.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,9 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 plugins {
     `java-library`
     `maven-publish`
+    id("eclipse")
     id("com.github.hierynomus.license") version "0.16.1"
-    id("io.github.goooler.shadow") version "8.1.7"
+    id("com.gradleup.shadow") version "8.3.8"
 }
 
 group = "me.clip"
@@ -14,10 +15,8 @@ description = "An awesome placeholder provider!"
 
 repositories {
     maven("https://oss.sonatype.org/content/repositories/snapshots/")
-
     mavenCentral()
     mavenLocal()
-
     maven("https://repo.codemc.org/repository/maven-public/")
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://repo.papermc.io/repository/maven-public/")
@@ -26,40 +25,31 @@ repositories {
 dependencies {
     implementation("org.bstats:bstats-bukkit:3.0.1")
     implementation("net.kyori:adventure-platform-bukkit:4.3.3")
-
-    //compileOnly("org.spigotmc:spigot-api:1.21-R0.1-SNAPSHOT")
     compileOnly("dev.folia:folia-api:1.20.1-R0.1-SNAPSHOT")
     compileOnlyApi("org.jetbrains:annotations:23.0.0")
-
     testImplementation("org.openjdk.jmh:jmh-core:1.32")
     testImplementation("org.openjdk.jmh:jmh-generator-annprocess:1.32")
-
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
-
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
-
     withJavadocJar()
     withSourcesJar()
-
     disableAutoTargetJvm()
 }
 
 license {
     header = rootProject.file("config/headers/main.txt")
-
     include("**/*.java")
     mapping("java", "JAVADOC_STYLE")
-
     encoding = "UTF-8"
-
     ext {
         set("year", 2024)
     }
+    ignoreFailures = true
 }
 
 val javaComponent: SoftwareComponent = components["java"]
@@ -80,7 +70,6 @@ tasks {
 
     withType<Javadoc> {
         isFailOnError = false
-
         with(options as StandardJavadocDocletOptions) {
             addStringOption("Xdoclint:none", "-quiet")
             addStringOption("encoding", "UTF-8")
@@ -90,11 +79,13 @@ tasks {
 
     withType<ShadowJar> {
         archiveClassifier.set("")
-
         relocate("org.bstats", "me.clip.placeholderapi.metrics")
         relocate("net.kyori", "me.clip.placeholderapi.libs.kyori")
-
         exclude("META-INF/versions/**")
+    }
+
+    withType<nl.javadude.gradle.plugins.license.License> {
+        isIgnoreFailures = true
     }
 
     test {
@@ -108,7 +99,6 @@ tasks {
                 from(javaComponent)
             }
         }
-
         repositories {
             maven {
                 if ("-DEV" in version.toString()) {
@@ -116,7 +106,6 @@ tasks {
                 } else {
                     url = uri("https://repo.extendedclip.com/releases")
                 }
-
                 credentials {
                     username = System.getenv("JENKINS_USER")
                     password = System.getenv("JENKINS_PASS")
@@ -133,3 +122,4 @@ configurations {
         extendsFrom(compileOnly.get())
     }
 }
+
